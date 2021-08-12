2da9e21a225aa34a869f36de366715a61fb168fb
Fernando Lepe <tenkla123@gmail.com>
2021-08-12 01:13:36 -0400

    correcion al controlador del login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
